fbfb803415f2673e3f68bb49561a6abb2c3e2074
Rian Pollock <rian@lsi.co.uk>
2025-03-06 15:20:45 +0000

    first commit

---
+++ src/images/index.js
@@ -0,0 +1,517 @@
+import A0 from "./LSi_1.png";
+import A1 from "./LSi_2.png";
+import A2 from "./LSi_3.png";
+import A3 from "./LSi_4.png";
+import A4 from "./LSi_5.png";
+import A5 from "./LSi_6.png";
+import A6 from "./LSi_7.png";
+import A7 from "./LSi_8.png";
+import A8 from "./LSi_9.png";
+import A9 from "./LSi_10.png";
+
+import A10 from "./LSi_11.png";
+import A11 from "./LSi_12.png";
+import A12 from "./LSi_13.png";
+import A13 from "./LSi_14.png";
+import A14 from "./LSi_15.png";
+import A15 from "./LSi_16.png";
+import A16 from "./LSi_17.png";
+import A17 from "./LSi_18.png";
+import A18 from "./LSi_19.png";
+import A19 from "./LSi_20.png";
+
+import A20 from "./LSi_21.png";
+import A21 from "./LSi_22.png";
+import A22 from "./LSi_23.png";
+import A23 from "./LSi_24.png";
+import A24 from "./LSi_25.png";
+import A25 from "./LSi_26.png";
+import A26 from "./LSi_27.png";
+import A27 from "./LSi_28.png";
+import A28 from "./LSi_29.png";
+import A29 from "./LSi_30.png";
+
+import A30 from "./LSi_31.png";
+import A31 from "./LSi_32.png";
+import A32 from "./LSi_33.png";
+import A33 from "./LSi_34.png";
+import A34 from "./LSi_35.png";
+import A35 from "./LSi_36.png";
+import A36 from "./LSi_37.png";
+import A37 from "./LSi_38.png";
+import A38 from "./LSi_39.png";
+import A39 from "./LSi_40.png";
+
+import A40 from "./LSi_41.png";
+import A41 from "./LSi_42.png";
+import A42 from "./LSi_43.png";
+import A43 from "./LSi_44.png";
+import A44 from "./LSi_45.png";
+import A45 from "./LSi_46.png";
+import A46 from "./LSi_47.png";
+import A47 from "./LSi_48.png";
+import A48 from "./LSi_49.png";
+import A49 from "./LSi_50.png";
+
+import A50 from "./LSi_51.png";
+import A51 from "./LSi_52.png";
+import A52 from "./LSi_53.png";
+import A53 from "./LSi_54.png";
+import A54 from "./LSi_55.png";
+import A55 from "./LSi_56.png";
+import A56 from "./LSi_57.png";
+import A57 from "./LSi_58.png";
+import A58 from "./LSi_59.png";
+import A59 from "./LSi_60.png";
+
+import A60 from "./LSi_61.png";
+import A61 from "./LSi_62.png";
+import A62 from "./LSi_63.png";
+import A63 from "./LSi_64.png";
+import A64 from "./LSi_65.png";
+import A65 from "./LSi_66.png";
+import A66 from "./LSi_67.png";
+import A67 from "./LSi_68.png";
+import A68 from "./LSi_69.png";
+import A69 from "./LSi_70.png";
+
+import A70 from "./LSi_71.png";
+import A71 from "./LSi_72.png";
+import A72 from "./LSi_73.png";
+import A73 from "./LSi_74.png";
+import A74 from "./LSi_75.png";
+import A75 from "./LSi_76.png";
+import A76 from "./LSi_77.png";
+import A77 from "./LSi_78.png";
+import A78 from "./LSi_79.png";
+import A79 from "./LSi_80.png";
+
+import A80 from "./LSi_81.png";
+import A81 from "./LSi_82.png";
+import A82 from "./LSi_83.png";
+import A83 from "./LSi_84.png";
+import A84 from "./LSi_85.png";
+import A85 from "./LSi_86.png";
+import A86 from "./LSi_87.png";
+import A87 from "./LSi_88.png";
+import A88 from "./LSi_89.png";
+import A89 from "./LSi_90.png";
+
+import A90 from "./LSi_91.png";
+import A91 from "./LSi_92.png";
+import A92 from "./LSi_93.png";
+import A93 from "./LSi_94.png";
+import A94 from "./LSi_95.png";
+import A95 from "./LSi_96.png";
+import A96 from "./LSi_97.png";
+import A97 from "./LSi_98.png";
+import A98 from "./LSi_99.png";
+import A99 from "./LSi_100.png";
+
+import A100 from "./LSi_101.png";
+import A101 from "./LSi_102.png";
+import A102 from "./LSi_103.png";
+import A103 from "./LSi_104.png";
+import A104 from "./LSi_105.png";
+import A105 from "./LSi_106.png";
+import A106 from "./LSi_107.png";
+import A107 from "./LSi_108.png";
+import A108 from "./LSi_109.png";
+import A109 from "./LSi_110.png";
+
+import A110 from "./LSi_111.png";
+import A111 from "./LSi_112.png";
+import A112 from "./LSi_113.png";
+import A113 from "./LSi_114.png";
+import A114 from "./LSi_115.png";
+import A115 from "./LSi_116.png";
+import A116 from "./LSi_117.png";
+import A117 from "./LSi_118.png";
+import A118 from "./LSi_119.png";
+import A119 from "./LSi_120.png";
+
+import A120 from "./LSi_121.png";
+import A121 from "./LSi_122.png";
+import A122 from "./LSi_123.png";
+import A123 from "./LSi_124.png";
+import A124 from "./LSi_125.png";
+import A125 from "./LSi_126.png";
+import A126 from "./LSi_127.png";
+import A127 from "./LSi_128.png";
+import A128 from "./LSi_129.png";
+import A129 from "./LSi_130.png";
+
+import A130 from "./LSi_131.png";
+import A131 from "./LSi_132.png";
+import A132 from "./LSi_133.png";
+import A133 from "./LSi_134.png";
+import A134 from "./LSi_135.png";
+import A135 from "./LSi_136.png";
+import A136 from "./LSi_137.png";
+import A137 from "./LSi_138.png";
+import A138 from "./LSi_139.png";
+import A139 from "./LSi_140.png";
+
+import A140 from "./LSi_141.png";
+import A141 from "./LSi_142.png";
+import A142 from "./LSi_143.png";
+import A143 from "./LSi_144.png";
+import A144 from "./LSi_145.png";
+import A145 from "./LSi_146.png";
+import A146 from "./LSi_147.png";
+import A147 from "./LSi_148.png";
+import A148 from "./LSi_149.png";
+import A149 from "./LSi_150.png";
+
+import A150 from "./LSi_151.png";
+import A151 from "./LSi_152.png";
+import A152 from "./LSi_153.png";
+import A153 from "./LSi_154.png";
+import A154 from "./LSi_155.png";
+import A155 from "./LSi_156.png";
+import A156 from "./LSi_157.png";
+import A157 from "./LSi_158.png";
+import A158 from "./LSi_159.png";
+import A159 from "./LSi_160.png";
+
+import A160 from "./LSi_161.png";
+import A161 from "./LSi_162.png";
+import A162 from "./LSi_163.png";
+import A163 from "./LSi_164.png";
+import A164 from "./LSi_165.png";
+import A165 from "./LSi_166.png";
+import A166 from "./LSi_167.png";
+import A167 from "./LSi_168.png";
+import A168 from "./LSi_169.png";
+import A169 from "./LSi_170.png";
+
+import A170 from "./LSi_171.png";
+import A171 from "./LSi_172.png";
+import A172 from "./LSi_173.png";
+import A173 from "./LSi_174.png";
+import A174 from "./LSi_175.png";
+import A175 from "./LSi_176.png";
+import A176 from "./LSi_177.png";
+
+import A177 from "./LSi_178.png";
+import A178 from "./LSi_179.png";
+import A179 from "./LSi_180.png";
+import A180 from "./LSi_181.png";
+import A181 from "./LSi_182.png";
+import A182 from "./LSi_183.png";
+import A183 from "./LSi_184.png";
+import A184 from "./LSi_185.png";
+import A185 from "./LSi_186.png";
+import A186 from "./LSi_187.png";
+
+import A187 from "./LSi_188.png";
+import A188 from "./LSi_189.png";
+import A189 from "./LSi_190.png";
+import A190 from "./LSi_191.png";
+import A191 from "./LSi_192.png";
+import A192 from "./LSi_193.png";
+import A193 from "./LSi_194.png";
+import A194 from "./LSi_195.png";
+import A195 from "./LSi_196.png";
+import A196 from "./LSi_197.png";
+
+import A197 from "./LSi_198.png";
+import A198 from "./LSi_199.png";
+import A199 from "./LSi_200.png";
+import A200 from "./LSi_201.png";
+import A201 from "./LSi_202.png";
+import A202 from "./LSi_203.png";
+import A203 from "./LSi_204.png";
+
+import A204 from "./LSi_205.png";
+import A205 from "./LSi_206.png";
+import A206 from "./LSi_207.png";
+import A207 from "./LSi_208.png";
+import A208 from "./LSi_209.png";
+import A209 from "./LSi_210.png";
+import A210 from "./LSi_211.png";
+import A211 from "./LSi_212.png";
+import A212 from "./LSi_213.png";
+import A213 from "./LSi_214.png";
+
+import A214 from "./LSi_215.png";
+import A215 from "./LSi_216.png";
+import A216 from "./LSi_217.png";
+import A217 from "./LSi_218.png";
+import A218 from "./LSi_219.png";
+import A219 from "./LSi_220.png";
+import A220 from "./LSi_221.png";
+import A221 from "./LSi_222.png";
+import A222 from "./LSi_223.png";
+import A223 from "./LSi_224.png";
+
+import A224 from "./LSi_225.png";
+import A225 from "./LSi_226.png";
+import A226 from "./LSi_227.png";
+import A227 from "./LSi_228.png";
+import A228 from "./LSi_229.png";
+import A229 from "./LSi_230.png";
+import A230 from "./LSi_231.png";
+import A231 from "./LSi_232.png";
+
+
+
+
+
+export const AirPods = Array.of(
+  A0,
+  A1,
+  A2,
+  A3,
+  A4,
+  A5,
+  A6,
+  A7,
+  A8,
+  A9,
+
+  A10,
+  A11,
+  A12,
+  A13,
+  A14,
+  A15,
+  A16,
+  A17,
+  A18,
+  A19,
+
+  A20,
+  A21,
+  A22,
+  A23,
+  A24,
+  A25,
+  A26,
+  A27,
+  A28,
+  A29,
+
+  A30,
+  A31,
+  A32,
+  A33,
+  A34,
+  A35,
+  A36,
+  A37,
+  A38,
+  A39,
+
+  A40,
+  A41,
+  A42,
+  A43,
+  A44,
+  A45,
+  A46,
+  A47,
+  A48,
+  A49,
+
+  A50,
+  A51,
+  A52,
+  A53,
+  A54,
+  A55,
+  A56,
+  A57,
+  A58,
+  A59,
+
+  A60,
+  A61,
+  A62,
+  A63,
+  A64,
+  A65,
+  A66,
+  A67,
+  A68,
+  A69,
+
+  A70,
+  A71,
+  A72,
+  A73,
+  A74,
+  A75,
+  A76,
+  A77,
+  A78,
+  A79,
+
+  A80,
+  A81,
+  A82,
+  A83,
+  A84,
+  A85,
+  A86,
+  A87,
+  A88,
+  A89,
+
+  A90,
+  A91,
+  A92,
+  A93,
+  A94,
+  A95,
+  A96,
+  A97,
+  A98,
+  A99,
+
+  A100,
+  A101,
+  A102,
+  A103,
+  A104,
+  A105,
+  A106,
+  A107,
+  A108,
+  A109,
+
+  A110,
+  A111,
+  A112,
+  A113,
+  A114,
+  A115,
+  A116,
+  A117,
+  A118,
+  A119,
+
+  A120,
+  A121,
+  A122,
+  A123,
+  A124,
+  A125,
+  A126,
+  A127,
+  A128,
+  A129,
+
+  A130,
+  A131,
+  A132,
+  A133,
+  A134,
+  A135,
+  A136,
+  A137,
+  A138,
+  A139,
+
+  A140,
+  A141,
+  A142,
+  A143,
+  A144,
+  A145,
+  A146,
+  A147,
+  A148,
+  A149,
+
+  A150,
+  A151,
+  A152,
+  A153,
+  A154,
+  A155,
+  A156,
+  A157,
+  A158,
+  A159,
+
+  A160,
+  A161,
+  A162,
+  A163,
+  A164,
+  A165,
+  A166,
+  A167,
+  A168,
+  A169,
+
+  A170,
+  A171,
+  A172,
+  A173,
+  A174,
+  A175,
+  A176,
+
+  A177,
+  A178,
+  A179,
+  A180,
+  A181,
+  A182,
+  A183,
+  A184,
+  A185,
+  A186,
+
+  A187,
+  A188,
+  A189,
+  A190,
+  A191,
+  A192,
+  A193,
+  A194,
+  A195,
+  A196,
+
+  A197,
+  A198,
+  A199,
+  A200,
+  A201,
+  A202,
+  A203,
+
+  A204,
+  A205,
+  A206,
+  A207,
+  A208,
+  A209,
+  A210,
+  A211,
+  A212,
+  A213,
+
+  A214,
+  A215,
+  A216,
+  A217,
+  A218,
+  A219,
+  A220,
+  A221,
+  A222,
+  A223,
+
+  A224,
+  A225,
+  A226,
+  A227,
+  A228,
+  A229,
+  A230,
+  A231,
+);
